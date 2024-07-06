365f3595ed8bcb8758a494d1bd12297bfa1f7056
Iaroslava <floweryaroslava@mail.ru>
2024-07-07 00:55:30 +0800

    Fix Finish 7

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,10 +4,15 @@ import getRandomInteger from '../utils.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  for (let i = 2; i < num; i += 1) {
-    if (num % i === 0) return false;
+  if (num <= 1) {
+    return false;
   }
-  return num !== 1;
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
 };
 
 const generatQuestionAndAnswer = () => {
